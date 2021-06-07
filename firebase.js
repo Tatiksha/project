c4f443b7335ea5a32dfa283cd0fff5d0aac1779c
Tatiksha <tatikshauniyal025@gmail.com>
2021-06-07 17:32:33 +0530

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYqhpRNU0aCDGIAPURnrpGA6gqg-F4ruc",
+    authDomain: "chat-app-be61c.firebaseapp.com",
+    projectId: "chat-app-be61c",
+    storageBucket: "chat-app-be61c.appspot.com",
+    messagingSenderId: "724963166445",
+    appId: "1:724963166445:web:d356a6b84c7411d246e4e4"
+  };
